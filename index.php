8ccf19ab5216c088029445e3d900909e34f94a80
thomas <brownaformac@gmail.com>
2018-03-26 15:14:32 +0200

    first

---
+++ index.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Templatemonster</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+    <meta charset="utf-8">
+</head>
+<body>
+
+    <section class="part1 container-fluid">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+            <a class="navbar-brand lien1 offset-md-2" href="#"><h1>Langerba</h1></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse offset-md-1" id="navbarText">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link lien1" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link lien" href="#">About us</a>
+                    </li>
+                    <li class="nav-item">
+                        <div class="dropdown lien">
+                             <a class="nav-link lien" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              Courses
+                            </a>                
+                            
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                                <!-- Default dropright button -->
+                                <div class="btn-group dropright">
+                                    <button type="button" class="btn btn-secondary">English</button>
+                                    <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <span class="sr-only">Toggle Dropright</span>
+                                    </button>
+                                    <div class="dropdown-menu">
+                                    <!-- Dropdown menu links -->
+                                    <a class="nav-link" href="#">For Kids</a>
+                                    <a class="nav-link" href="#">For School Children</a>
+                                    <a class="nav-link" href="#">For Graduates</a>
+                                    <a class="nav-link" href="#">For Adults</a>
+                                    </div>
+                                </div>
+                                <button class="dropdown-item" type="button">German</button>
+                                <button class="dropdown-item" type="button">French</button>
+                                <button class="dropdown-item" type="button">Itilian</button>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link lien" href="#">Gallery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link lien" href="#">Contact us</a>
+                    </li>
+                </ul>
+                <ul class="col-md-6">
+                    <li class="navbar-text lientel">
+                        +1 800 598 78 87
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </section>
+
+
+        <div class="container-fluid top back1">
+            <div class="col-xs-12 col-lg-12">
+                <h2 class="">Open the world of opportunities!</h2>
+                <div class="">Learn to communicate with the world around you.</div>   
+                <div class="">
+                    <a href="#" class="myButton">REGISTER NOW</a>
+                </div> 
+            </div>      
+        </div>
+
+
+
+    
+        <div class="back2 container-fluid">
+            <div class="row">
+                <div class="col-sm-12 text-center offset-md-2 col-md-3 textback2">
+                    <h2 class="titreback2">Studying Foreign Languages</h2>
+                    <h3 class="titreback2">Every foreign language has its own momentum</h3>
+                    <div class="titreback2">The more you learn, the more you want to learn. Say you love Italian food and decide to study Italian. At first it's hard. But one day you meet some Italian tourists on the bus and you understand enough to point them towards their hotel. Before you part ways, they give you their e-mail address. Now you can easily communicate with them and study some more.</div>
+                    <div>
+                        <a href="#" class="myButton">REGISTER NOW</a>
+                    </div>
+                </div>
+                <div class="col-sm-12 offset-md-1 col-md-6 imgback2">
+                    <img class="img-fluid" src="img/img02.png">
+                </div>    
+            </div>             
+        </div>
+  
+
+
+    <div class="container-fluid">
+        <div>
+            <h2 class="col-lg-12 titreback3">6 reasons to start learn language</h2>
+
+            <div class="row rowback3">
+                <div class="col-sm-12 text-center offset-md-2 col-md-3">
+                    <img src="img/iconeplanet.png" class="icone">
+                    <h3>English is spoken all over the world</h3>
+                    <div>
+                        English is not the most widely spoken language (that title goes to Chinese), but it does come in third, after Spanish. And the number of 335 million native speakers is respectable.
+                    </div>
+                </div>
+                <div class="col-sm-12 text-center offset-md-1 col-md-4">
+                    <img src="img/iconeplanet.png" class="icone">
+                    <h3>International business requires English</h3>
+                    <div>
+                        In terms of international commerce, English is the language to learn. When the Chinese do business in Egypt, the negotiations and transactions happen in English.
+                    </div>
+                </div>                
+            </div>
+
+            <div class="row rowback3">
+                <div class="col-sm-12 text-center offset-md-2 col-md-3">
+                    <img src="img/iconeplanet.png" class="icone">
+                    <h3>Education and research opportunities</h3>
+                    <div>
+                        Thinking of studying abroad or publishing a research paper? Chances are you will decide to learn English at some point along the way, or to study at your chosen institution.
+                    </div>
+                </div>
+                <div class="col-sm-12 text-center offset-md-1 col-md-4">
+                    <img src="img/iconeplanet.png" class="icone">
+                    <h3>A perfect language for traveling</h3>
+                    <div>
+                        If you travel often, you’ll note signs tend to come in the local tongue first and English second. That’s because English is considered the most popular international language.
+                    </div>
+                </div>                
+            </div>
+
+            <div class="row rowback3">
+                <div class="col-sm-12 text-center offset-md-2 col-md-3">
+                    <img src="img/iconeplanet.png" class="icone">
+                    <h3>The language of cinema and music</h3>
+                    <div>
+                        If you like going to the movies and are a fan of sitcoms, then English should be on your list. That’s because so many of the world’s films are written and produced in English.
+                    </div>
+                </div>
+                <div class="col-sm-12 text-center offset-md-1 col-md-4">
+                    <img src="img/iconeplanet.png" class="icone">
+                    <h3>Hemingway and Harry Potter</h3>
+                    <div>
+                        English has been around for a long time and English speaking writers have always been a fairly prolific bunch, both in Great Britain and the United States..
+                    </div>
+                </div>                
+            </div>
+
+
+    <section class="back4">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="offset-md-2 col-md-3 imgback4">
+                    <img class="img-fluid" src="img/img03.png">
+                </div>
+                <div class="offset-sm-1 col-sm-10 offset-md-1 col-md-3 textback4">
+                    <h2 class="titreback4">Our Book Center is an official distributor of Oxford University Press</h2>
+                    <h3 class="titreback4">We offer a wide range of books for teachers and students.</h3>
+                    <div class="titreback4">
+                        The Book Center provides schools, language centers, colleges and universities with English teaching materials from Seattle to Miami. Our dealers work in almost all states.
+                    </div> 
+                </div>
+            </div>           
+        </div>
+    </section>
+
+    <section class="back5">
+        <div class="container-fluid">
+            <div class="col-md-12 text-center textback5">
+                <h2 class="titreback5">Get your free e-mail course!</h2>
+                <div class="titreback5">30 lessons per week, Upper-Intermediate to Advanced levels, course length: 4, 8-12 weeks</div>
+                <div class="row">
+                    <div class="col-md-12 titreback5">
+                       <input type="text" class="css-input col-sm-2" />
+                        <a href="#" class="myButton">I WANT A COURSE</a>
+                    </div>  
+                </div>  
+            </div>            
+        </div>
+    </section>
+
+
+ <section class="back6 text-center textback6">
+        <div class="container">
+                <h2 class="">What our students are saying</h2>
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" data-interval ="false">
+                    <ol class="carousel-indicators barcar">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active offset-2 col-md-8">
+                            <img src="img/apostrophe.png">
+                                    <p>
+                                        <q>Two unforgettable weeks in Langerba! I would like to thank all the teachers for their positive spirit, amazing lessons and high professionalism. i will miss our lessons!</q>
+                                    </p>                       
+                                    <cite>Anthony Little</cite>                        
+                        </div>
+                        <div class="carousel-item offset-2 col-md-8">
+                            <img src="img/apostrophe.png">
+                                    <p>
+                                        <q>I just can praise the peofessionalism and kindness of the staff working here. They advised me on the best courses for my level and allowed me to change them at any time. Thank you all.</q>
+                                    </p>
+                                    <cite>Thomas Austin</cite>
+                                </div>
+                         
+                        </div>
+                        <div class="carousel-item offset-2 col-md-8">
+                            <img src="img/apostrophe.png">
+                                <p>
+                                    <q>I enjoyed my experience with you. It was worth every spent dollar. More than just the language, I learned about so many different cultures and made friends from all over the world.</q>
+                                </p>
+                                <cite>Paticia Cruz</cite>            
+                        </div>
+                        <div class="carousel-item offset-2 col-md-8">
+                            <img src="img/apostrophe.png">
+                                <p>
+                                    <q>Your business program, the flexibility with scheduling, the teachers and directors are very easygoing. The location is perfect, the prices are excellent compared to other schools.</q>
+                                </p>
+                                <cite>Gary Montgomery</cite>
+                        </div>
+                        <div class="carousel-item offset-2 col-md-8">
+                            <img src="img/apostrophe.png">
+                                <p>
+                                    <q>My experience here was the best experience I've ever had, and I do realize the improvement in my language. Sometimes I surprise myself, because now I speak so fluently. Thank you very much!</q>
+                                </p>
+                                <cite>Helen Howard</cite>
+                        </div>
+                    </div>
+                </div>
+        </div>
+    </section>
+
+    <section class="back7 container-fluid">
+        <nav class="navbar navbar-expand-lg row">
+            <a class="navbar-brand lien1 offset-md-2" href="#"><h1>Langerba</h1></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse offset-md-1" id="navbarText">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link lien1" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link lienbot" href="#">About us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link lienbot" href="#">Courses</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link lienbot" href="#">Gallery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link lienbot" href="#">Contact us</a>
+                    </li>
+                </ul>
+                <ul class="col-md-6">
+                    <li class="navbar-text lientel">
+                        +1 800 598 78 87
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </section>
+
+
+
+
+    <section class="back8">
+        <div class="container-fluid">
+            <div class="offset-md-2 col-md-8 copyright textback8">
+                2016. All Langerba financings are subject to eligibility, credit and underwriting approval. The programs advertised on this page are not a commitment or guarantee from Langerba to provide funds. Programs, rates and other terms & conditions on this page are subject to change without notice. <span class="der">Privacy Policy</span>
+        </div>
+    </section>
+     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="  anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+  </body>
+</html>
